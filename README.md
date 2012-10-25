745923592cd148f4c5649f65724f51100833587d
José F. Romaniello <jfromaniello@gmail.com>
2012-10-25 10:08:05 -0300

    initial import from singly project

---
+++ README.md
@@ -0,0 +1,93 @@
+OData query string parser for node.js based on [pegjs](http://pegjs.majda.cz/).
+
+## Installation
+
+```
+npm install odata-parser
+```
+
+## Usage
+
+```javascript
+var parser = require("odata-parser");
+
+var ast = parser.parse("$top=10&$skip=5&$select")
+
+util.inspect(ast)
+```
+
+will result in:
+
+```json
+{ 
+  '$top': 10,
+  '$skip': 5,
+  '$select': [ 'foo' ] 
+}
+```
+
+Filters like:
+
+```javascript
+parser.parse("$filter=Name eq 'John' and LastName lt 'Doe'")
+```
+results in:
+
+```json
+
+{
+    $filter: {
+        type: 'and',
+        left: {
+            type: 'eq',
+            left: {
+                type: 'property',
+                name: 'Name'
+            },
+            right: {
+                type: 'literal',
+                value: 'John'
+            }
+        },
+        right: {
+            type: 'lt',
+            left: {
+                type: 'property',
+                name: 'LastName'
+            },
+            right: {
+                type: 'literal',
+                value: 'Doe'
+            }
+        }
+    }
+}
+```
+
+Using functions in filters
+
+```javascript
+parser.parse("$filter=substringof('nginx', Servers)")
+```
+
+restuls in:
+
+```json
+{
+    $filter: {
+        type: 'functioncall',
+        func: 'substringof',
+        args: [{
+            type: 'literal',
+            value: 'nginx'
+        }, {
+            type: 'property',
+            name: 'Servers'
+        }]
+    }
+}
+```
+
+## License
+
+MIT - qraftlabs - 2012
---
+++ test/parser.specs.js
@@ -0,0 +1,173 @@
+var assert = require('assert');
+var parser = require("../lib");
+
+describe('odata.parser grammar', function () {
+
+    it('should parse $top and return the value', function () {
+
+        var ast = parser.parse('$top=40');
+
+        assert.equal(ast.$top, 40);
+    });
+
+    it('should parse two params', function () {
+
+        var ast = parser.parse('$top=4&$skip=5');
+
+        assert.equal(ast.$top, 4);
+        assert.equal(ast.$skip, 5);
+    });
+
+
+    it('should parse three params', function () {
+
+        var ast = parser.parse('$top=4&$skip=5&$select=Rating');
+
+        assert.equal(ast.$top, 4);
+        assert.equal(ast.$skip, 5);
+        assert.equal(ast.$select[0], "Rating");
+    });
+
+    it('should parse string params', function () {
+
+        var ast = parser.parse('$select=Rating');
+
+        assert.equal(ast.$select[0], 'Rating');
+    });
+
+    it('should accept * in $select', function () {
+
+        var ast = parser.parse('$select=*');
+
+        assert.equal(ast.$select[0], '*');
+    });
+
+    it('should accept * and , and / in $select', function () {
+        
+        var ast = parser.parse('$select=*,Category/Name');
+
+        assert.equal(ast.$select[0], '*');
+        assert.equal(ast.$select[1], 'Category/Name');
+    });
+
+    it('should accept more than two fields', function () {
+
+        var ast = parser.parse('$select=Rating, Name,LastName');
+
+        assert.equal(ast.$select[0], 'Rating');
+        assert.equal(ast.$select[1], 'Name');
+        assert.equal(ast.$select[2], 'LastName');
+    });
+
+    // This select parameter is not currently supported.
+    it('should accept * after . in $select', function () {
+
+        var ast = parser.parse('$select=DemoService.*');
+
+        assert.equal(ast.$select[0], 'DemoService.*');
+    });
+    
+    it('should parse order by', function () {
+
+        var ast = parser.parse('$orderby=ReleaseDate desc, Rating');
+
+        assert.equal(ast.$orderby.ReleaseDate, "desc");
+        assert.equal(ast.$orderby.Rating, "asc");
+
+    });
+
+    it('should parse $filter', function () {
+
+        var ast = parser.parse("$filter=Name eq 'Jef'");
+
+        assert.equal(ast.$filter.type, "eq");
+        assert.equal(ast.$filter.left.type, "property");
+        assert.equal(ast.$filter.left.name, "Name");
+        assert.equal(ast.$filter.right.type, "literal");
+        assert.equal(ast.$filter.right.value, "Jef");
+    });
+    
+    it('should parse multiple conditions in a $filter', function () {
+
+        var ast = parser.parse("$filter=Name eq 'John' and LastName lt 'Doe'");
+
+        assert.equal(ast.$filter.type, "and");
+        assert.equal(ast.$filter.left.type, "eq");
+        assert.equal(ast.$filter.left.left.type, "property");
+        assert.equal(ast.$filter.left.left.name, "Name");
+        assert.equal(ast.$filter.left.right.type, "literal");
+        assert.equal(ast.$filter.left.right.value, "John");
+        assert.equal(ast.$filter.right.type, "lt");
+        assert.equal(ast.$filter.right.left.type, "property");
+        assert.equal(ast.$filter.right.left.name, "LastName");
+        assert.equal(ast.$filter.right.right.type, "literal");
+        assert.equal(ast.$filter.right.right.value, "Doe");
+    });
+
+    it('should parse substringof $filter', function () {
+
+        var ast = parser.parse("$filter=substringof('nginx', Data)");
+
+        assert.equal(ast.$filter.type, "functioncall");
+        assert.equal(ast.$filter.func, "substringof");
+        
+        assert.equal(ast.$filter.args[0].type, "literal");
+        assert.equal(ast.$filter.args[0].value, "nginx");
+
+        assert.equal(ast.$filter.args[1].type, "property");
+        assert.equal(ast.$filter.args[1].name, "Data");
+
+    });
+
+    it('should parse substringof eq true in $filter', function () {
+
+        var ast = parser.parse("$filter=substringof('nginx', Data) eq true");
+
+        assert.equal(ast.$filter.type, "eq");
+        
+
+        assert.equal(ast.$filter.left.type, "functioncall");
+        assert.equal(ast.$filter.left.func, "substringof");
+        assert.equal(ast.$filter.left.args[0].type, "literal");
+        assert.equal(ast.$filter.left.args[0].value, "nginx");
+        assert.equal(ast.$filter.left.args[1].type, "property");
+        assert.equal(ast.$filter.left.args[1].name, "Data");
+
+        assert.equal(ast.$filter.right.type, "literal");
+        assert.equal(ast.$filter.right.value, true);
+    });
+
+    it('should parse startswith $filter', function () {
+
+        var ast = parser.parse("$filter=startswith('nginx', Data)");
+
+        assert.equal(ast.$filter.type, "functioncall");
+        assert.equal(ast.$filter.func, "startswith");
+        
+        assert.equal(ast.$filter.args[0].type, "literal");
+        assert.equal(ast.$filter.args[0].value, "nginx");
+
+        assert.equal(ast.$filter.args[1].type, "property");
+        assert.equal(ast.$filter.args[1].name, "Data");
+
+    });
+   
+    it('should return an error if invalid value', function() {
+
+        var ast = parser.parse("$top=foo");
+
+        assert.equal(ast.error, "invalid $top parameter");
+    });
+    
+
+    it('should convert dates to javascript Date', function () {
+        var ast = parser.parse("$top=2&$filter=Date gt datetime'2012-09-27T21:12:59'");
+        assert.ok(ast.$filter.right.value instanceof Date);
+    });
+
+    // it('xxxxx', function () {
+    //     var ast = parser.parse("$top=2&$filter=Date gt datetime'2012-09-27T21:12:59'");
+
+    //     console.log(JSON.stringify(ast, 0, 2));
+    // });
+});
